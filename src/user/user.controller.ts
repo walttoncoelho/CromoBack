a52cea5bcdec13aca233a8e15d4ff8016c01e108
ciroDourado <ciro.brz@gmail.com>
2023-03-07 22:09:09 -0300

    feat: configura prisma e cria modulo de usuarios

---
+++ src/user/user.controller.ts
@@ -0,0 +1,50 @@
+import { 
+  Body, 
+  Controller, 
+  Get, 
+  Param, 
+  Post, 
+  Put
+} from "@nestjs/common";
+
+@Controller("users")
+export class UserController {
+
+  @Post()
+  async create(@Body() data) {
+    let user = {
+      name: data.name,
+      email: data.email,
+      password: data.password
+    };
+    return { user };
+  }
+
+  @Get()
+  async all() {
+    let users: [];
+    return { users };
+  }
+
+  @Get(":id")
+  async show(@Param() params) {
+    let id = params.id;
+    let user = {
+      id
+    };
+    return { user };
+  }
+
+  @Put()
+  async update(@Body() data, @Param() params) {
+    let id = params.id;
+    let user = {
+      id,
+      name: data.name,
+      email: data.email,
+      password: data.password
+    };
+    return { user };
+  }
+
+}
