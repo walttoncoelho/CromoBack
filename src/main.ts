7122da7148c3ef3f8b9f442df90c4cc0c7f22ab2
ciroDourado <ciro.brz@gmail.com>
2023-05-03 23:57:29 -0300

    feat: configura CORS

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,9 @@ import { LogInterceptor } from './interceptors/log.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: [ /* domínio da aplicação Front do CROMO */ ]
+  });
   app.useGlobalPipes(new ValidationPipe);
   app.useGlobalInterceptors(new LogInterceptor)
   await app.listen(3000);
