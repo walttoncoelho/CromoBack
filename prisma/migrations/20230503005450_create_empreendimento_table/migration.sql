d0bf8e6a732c3b92af4e3d8cac40d0286084d311
ciroDourado <ciro.brz@gmail.com>
2023-05-06 22:56:06 -0300

    fix: generaliza o nome da foto do empreendimento

--- prisma/migrations/20230503005450_create_empreendimento_table/migration.sql
+++ prisma/migrations/20230503005450_create_empreendimento_table/migration.sql
@@ -49,7 +49,6 @@ CREATE TABLE `FotoEmpreendimento` (
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
 
-    UNIQUE INDEX `FotoEmpreendimento_arquivo_key`(`arquivo`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
