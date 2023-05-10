b67b4afd6b5f47e866cea3c9ab51f8e7820fb0fc
ciroDourado <ciro.brz@gmail.com>
2023-05-09 22:31:39 -0300

    fix: corrige tipo de envio de valores de numeros

--- src/modules/numero/numero.presenter.ts
+++ src/modules/numero/numero.presenter.ts
@@ -3,19 +3,19 @@ import { Numero } from "@prisma/client";
 export class NumeroPresenter {
   titulo: string;
   descricao: string;
-  lotes: string;
-  asfalto: string;
-  rua: string;
-  familias: string;
+  lotes: number;
+  asfalto: number;
+  rua: number;
+  familias: number;
 
   constructor(
     numero: Numero
   ) {
     this.titulo = numero.titulo;
     this.descricao = numero.descricao;
-    this.lotes = `${numero.lotes}`;
-    this.asfalto = `${numero.asfalto}`;
-    this.rua = `${numero.rua}`;
-    this.familias = `${numero.familias}`;
+    this.lotes = numero.lotes;
+    this.asfalto = numero.asfalto;
+    this.rua = numero.rua;
+    this.familias = numero.familias;
   }
 }
