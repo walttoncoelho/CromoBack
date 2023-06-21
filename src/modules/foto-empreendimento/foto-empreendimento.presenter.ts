e26e3847063668ca61e4b5dbbab61b81fa2da799
ciroDourado <ciro.brz@gmail.com>
2023-06-21 00:49:41 -0300

    refact: retorna id da imagem

--- src/modules/foto-empreendimento/foto-empreendimento.presenter.ts
+++ src/modules/foto-empreendimento/foto-empreendimento.presenter.ts
@@ -2,11 +2,13 @@ import { FotoEmpreendimento } from "@prisma/client";
 import { applicationUrl } from "src/main";
 
 export class FotoEmpreendimentoPresenter {
+  id: number;
   imagem: string;
 
   constructor(
     fotoEmpreendimento: FotoEmpreendimento,
   ) {
+    this.id = fotoEmpreendimento.id;
     this.imagem = `${applicationUrl}/empreendimentos/${fotoEmpreendimento.empreendimentoId}/galeria/${fotoEmpreendimento.arquivo}`
   }
 }
